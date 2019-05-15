21d3a87eed844033b750e102fe992317a857e167
George Chang <georgec1104@gmail.com>
2019-05-15 20:22:32 +0800

    use props for property of Pet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = function() {
+const Pet = props => {
 	return React.createElement("div", {}, [
-		React.createElement("h1", {}, "Luna"),
-		React.createElement("h2", {}, "Dog"),
-		React.createElement("h3", {}, "Havanese")
+		React.createElement("h1", {}, props.name),
+		React.createElement("h2", {}, props.animal),
+		React.createElement("h3", {}, props.breed)
 	]);
 };
 
 const App = () => {
 	return React.createElement("div", {}, [
 		React.createElement("h1", {}, "Adopt Me!"),
-		React.createElement(Pet),
-		React.createElement(Pet),
-		React.createElement(Pet)
+		React.createElement(Pet, {
+			name: "Luna",
+			animal: "Dog",
+			breed: "Japanese"
+		}),
+		React.createElement(Pet, {
+			name: "Niny",
+			animal: "Dog",
+			breed: "Japanese"
+		}),
+		React.createElement(Pet, {
+			name: "Yami",
+			animal: "Bird",
+			breed: "mixed"
+		})
 	]);
 };
 
