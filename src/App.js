f7c38b8fce74a3901727d394065abc18a08ffcab
George Chang <georgec1104@gmail.com>
2019-05-16 17:09:13 +0800

    pull and use petfinder data to show on page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,78 @@ import { render } from "react-dom";
 import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+	key: process.env.API_KEY,
+	secret: process.env.API_SECRET
+});
+
 // class component
 class App extends React.Component {
-	handleTitleClick() {
-		alert("you clicked the title");
+	constructor(props) {
+		super(props);
+		this.state = {
+			pets: []
+		};
+	}
+	componentDidMount() {
+		petfinder.pet
+			.find({ output: "full", location: "Boston, MA" })
+			.then(data => {
+				let pets;
+
+				// Verify if data are coming in
+				if (data.petfinder.pets && data.petfinder.pets.pet) {
+					// see if the data from api contains multiple results in a Array
+					if (Array.isArray(data.petfinder.pets.pet)) {
+						pets = data.petfinder.pets.pet;
+						// if there is only 1 result, then put it in/as Array
+					} else {
+						pets = [data.petfinder.pets.pet];
+					}
+				} else {
+					pets = [];
+				}
+				// update the content in state
+				this.setState({ pets: pets });
+			});
 	}
 	render() {
 		return (
 			<div>
 				<h1>Adopt Me!</h1>
-				<Pet name="Luna" animal="dog" breed="Havanese" />
-				<Pet name="Leisy" animal="cat" breed="Blacky" />
-				<Pet name="Ginger" animal="fish" breed="Golden" />
+				<div>
+					{this.state.pets.map(pet => {
+						// sometimes the dog has multiple breeds
+						let breed;
+						if (Array.isArray(pet.breeds.breed)) {
+							breed = pet.breeds.breed.join(", ");
+						} else {
+							breed = pet.breeds.breed;
+						}
+						return (
+							<Pet
+								key={pet.id}
+								animal={pet.animal}
+								name={pet.name}
+								breed={breed}
+							/>
+						);
+					})}
+				</div>
 			</div>
 		);
+
+		// render() {
+		// 	return (
+		// 		<div>
+		// 			<h1>Adopt Me!</h1>
+		// 			{/* dump all the data as json on the page */}
+		// 			<pre>
+		// 				{/* the 4 means indent */}
+		// 				<code>{JSON.stringify(this.state, null, 4)}</code>
+		// 			</pre>
+		// 		</div>
+		// 	);
 		// return React.createElement("div", {}, [
 		// 	React.createElement(
 		// 		"h1",
@@ -42,7 +100,6 @@ class App extends React.Component {
 	}
 }
 render(React.createElement(App), document.getElementById("root"));
-
 // function component
 // const App = function() {};
 // const App = () => {
