00c96f279638c669a2d4161059e36fbb4a7d0aed
George Chang <georgec1104@gmail.com>
2019-05-16 17:11:24 +0800

    use class component for Pet

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-const Pet = props => {
-	return (
-		<div>
-			<h1>{props.name}</h1>
-			<h2>{props.animal}</h2>
-			<h3>{props.breed}</h3>
-		</div>
-	);
-};
+class Pet extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{props.name}</h1>
+				<h2>{props.animal}</h2>
+				<h3>{props.breed}</h3>
+			</div>
+		);
+	}
+}
+
 export default Pet;
